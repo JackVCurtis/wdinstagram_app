1beb2252be88bc6f679092ada8a5f88af9ce995a
JackCurtis <jack.v.curtis@gmail.com>
2015-01-11 18:31:51 -0500

    added date validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,12 @@
 class Entry < ActiveRecord::Base
   validates :author, presence: true
   validates :photo_url, presence: true
+  validate :before_start_date
+
+  def before_start_date
+    start_date = Date.new(2010, 10, 1)
+    unless ( start_date <=> date_taken ) == -1
+      errors.add(:date_taken, "taken too long ago")
+    end 
+  end
 end
